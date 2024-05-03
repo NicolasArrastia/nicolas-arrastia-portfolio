1d8ec74818779d9499b1f8fcc78dcf9b7b5af4f1
Nicolas <nicolasarrastia@gmail.com>
2024-05-03 12:14:11 -0300

    Enhance loading animation

--- src/app/LoaderAnimation/LoaderAnimation.tsx
+++ src/app/LoaderAnimation/LoaderAnimation.tsx
@@ -2,6 +2,8 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 
+const GENERAL_DELAY = 0.2;
+
 const LoaderAnimation = () => {
   const [isAnimationComplete, setIsAnimationComplete] = useState(false);
 
@@ -17,21 +19,17 @@ const LoaderAnimation = () => {
           className="fixed top-0 left-0 w-screen h-screen bg-black z-50"
           style={{ mixBlendMode: "multiply" }}
           animate={{ opacity: 0 }}
-          transition={{ delay: 0.8, duration: 0.1 }}
+          transition={{ delay: 0.8 + GENERAL_DELAY, duration: 0.2 }}
         >
           <motion.div
-            className=" bg-white absolute rounded-md -translate-x-1/2 -translate-y-1/2"
-            style={{}}
-            initial={{
-              top: "-10%",
-              left: "50%",
-              width: "2rem",
-              height: "2rem",
+            className="size-8 bg-white absolute rounded-md -translate-x-1/2 -translate-y-1/2 -top-1/2 left-1/2"
+            transition={{
+              delay: GENERAL_DELAY,
+              duration: 1,
             }}
-            transition={{ duration: 1 }}
             animate={{
               rotate: [0, 180, 360],
-              top: ["0%", "60%", "50%"],
+              top: ["-10%", "60%", "50%"],
               scale: [1, 2, 60],
             }}
           ></motion.div>
