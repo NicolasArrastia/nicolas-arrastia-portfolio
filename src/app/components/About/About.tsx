c810b8dea6d0f6478d0fcf12a864145efda8ee77
Nicolas <nicolasarrastia@gmail.com>
2024-05-04 02:13:28 -0300

    Add general layout for about section

---
+++ src/app/components/About/About.tsx
@@ -0,0 +1,28 @@
+"use client";
+import SectionLayout from "@/components/SectionLayout";
+import TitleText from "@/components/TitleText";
+import { motion } from "framer-motion";
+
+const DEFAULT_TRANSITION_SETTINGS = {
+  initial: { opacity: 0 },
+  viewport: { once: true },
+  transition: { duration: 0.75, delay: 0.5 },
+  whileInView: { opacity: 1 },
+};
+
+const About = () => {
+  return (
+    <SectionLayout>
+      <TitleText text="About" />
+      <motion.p {...DEFAULT_TRANSITION_SETTINGS}>about me here</motion.p>
+
+      <TitleText text="Hobbies" delay={1} />
+      <motion.p {...DEFAULT_TRANSITION_SETTINGS}>hobbies here</motion.p>
+
+      <TitleText text="Pets" delay={1.5} />
+      <motion.p {...DEFAULT_TRANSITION_SETTINGS}>and my pets here</motion.p>
+    </SectionLayout>
+  );
+};
+
+export default About;
