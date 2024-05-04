b49193ed5bed718e62ad21f378f7b553d5f39543
Nicolas <nicolasarrastia@gmail.com>
2024-05-04 02:10:40 -0300

    Add Title component with animation

---
+++ src/components/TitleText/TitleText.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+
+type Props = { text: string; delay?: number };
+
+const TitleText = ({ text, delay = 0.5 }: Props) => {
+  return (
+    <div className="w-fit relative p-1 -left-1 mb-4">
+      <motion.h2
+        className="text-4xl"
+        transition={{ duration: 0.2, delay: delay + delay / 4 }}
+        viewport={{ once: true }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+      >
+        {text}
+      </motion.h2>
+      <motion.div
+        initial={{ scaleX: 0, transformOrigin: "left" }}
+        transition={{ duration: 0.5, delay: delay, ease: "easeOut" }}
+        viewport={{ once: true }}
+        whileInView={{
+          scaleX: [0, 1, 1, 0],
+          translateX: [0, 0, 0, "100%"],
+        }}
+        className="absolute w-full left-0 top-0 bg-blue-500 h-full"
+      />
+    </div>
+  );
+};
+
+export default TitleText;
