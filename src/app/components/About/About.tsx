b5c8403fa1fe8d67c946a4c9571b1639b524dc7e
Nicolas <nicolasarrastia@gmail.com>
2024-05-06 02:16:56 -0300

    Add about correct data

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -14,7 +14,7 @@ import { PICTURES } from "./constants";
 const TEXT_TRANSITION = {
   initial: { opacity: 0 },
   viewport: { once: true },
-  transition: { duration: 0.75, delay: 0.5 },
+  transition: { duration: 1, delay: 0.5 },
   whileInView: { opacity: 1 },
 };
 
@@ -22,17 +22,39 @@ const About = () => {
   return (
     <SectionLayout>
       <TitleText text="About" />
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 text-justify">
         <div className="">
           {/* // TODO: Add sections inside about */}
           <motion.p {...TEXT_TRANSITION}>
-            Hi! I&apos;m Nico. A young web developer currently living in
-            Argentina. I am a highly curious and imaginative person that enjoys
-            learning constantly. I am passionate about technology, science,
-            languages and literature. Understanding how things around me work
-            and exploring different perspectives to solve problems motivates me.
-            Additionally, with knowledge in design, I can contribute visually on
-            different interfaces I work with.
+            Hi! I&apos;m <span className="text-blue-400">Nicolás Arrastía</span>
+            . A young web developer, currently living in{" "}
+            <span className="text-blue-400">Mar Chiquita, Argentina</span> with
+            my fiancée and our three pets. My native language is spanish, but I
+            don&apos;t have problem if speaking in english is necessary. I am
+            passionate about technology, science, languages and literature.
+          </motion.p>
+          <TitleText text="Why me?" size="text-2xl" />
+          <motion.p {...TEXT_TRANSITION}>
+            Because I am <span className="text-blue-400">respectful</span>,{" "}
+            <span className="text-blue-400">curious</span> and{" "}
+            <span className="text-blue-400">friendly</span> and I love solving
+            problems for other people to make their life easier; I enjoy
+            learning new things and I try to understand how things work. Also, I
+            have an interest in <span className="text-blue-400">design</span>,
+            that&apos;s why I took a course in ux/ui design to potentiate my
+            frontend skills with it.
+          </motion.p>
+          <TitleText text="Random facts about me" size="text-2xl" />
+          <motion.p {...TEXT_TRANSITION}>
+            My favorite color is <span className="text-blue-400">blue</span>; my
+            favorite animal is the <span className="text-blue-400">eagles</span>
+            ; I like <span className="text-blue-400">winter</span> because you
+            can enjoy a good moment drinking a hot drink while being with others
+            or while reading a good book. A place to visit could be a rural area
+            in <span className="text-blue-400">Japan</span>. And lastly, Between
+            day or night I prefer the night, because it&apos;s quiet and I can
+            keep learning about constellations and see how the stars, weird
+            mirrors of our universe, move through the sky as the time goes by.
           </motion.p>
         </div>
 
