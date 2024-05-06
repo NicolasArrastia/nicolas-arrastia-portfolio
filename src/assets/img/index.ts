187ee3df81845251048bb8326d4a7f8193448922
Nicolas <nicolasarrastia@gmail.com>
2024-05-06 00:39:01 -0300

    Add images

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,4 +1,15 @@
 import GreatWaveOfKanagawa from "./great_wave_of_kanagawa.jpg";
 import TodoApp from "./todo-app.jpg";
+import Kuma from "./kuma.jpg";
+import VaniAndI from "./vani-and-I.jpg";
+import CafeAndRobin from "./cafe-and-robin.jpg";
+import PileOfRocks from "./pile-of-rocks.jpg";
 
-export { GreatWaveOfKanagawa, TodoApp };
+export {
+  GreatWaveOfKanagawa,
+  TodoApp,
+  Kuma,
+  VaniAndI,
+  CafeAndRobin,
+  PileOfRocks,
+};
