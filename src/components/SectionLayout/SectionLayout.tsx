46ec0976c6199065fb0c1084fddab58903e1d668
Nicolas <nicolasarrastia@gmail.com>
2024-05-07 02:02:21 -0300

    Add id to section component

--- src/components/SectionLayout/SectionLayout.tsx
+++ src/components/SectionLayout/SectionLayout.tsx
@@ -1,11 +1,17 @@
+import { SectionIds } from "@/app/enum";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
+  id: SectionIds;
 };
 
-const SectionLayout = ({ children }: Props) => {
-  return <div className="px-4 md:px-16 py-8 bg-neutral-950">{children}</div>;
+const SectionLayout = ({ children, id }: Props) => {
+  return (
+    <div id={id} className="px-4 md:px-16 py-8">
+      {children}
+    </div>
+  );
 };
 
 export default SectionLayout;
