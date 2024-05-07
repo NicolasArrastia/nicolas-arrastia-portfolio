77a1b2720feb79b4d546217b4696e69f363dc5b2
Nicolas <nicolasarrastia@gmail.com>
2024-05-07 01:58:02 -0300

    Add skills images

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,4 +2,37 @@ import NicolasLogo from "./nicolas-arrastia-logo.svg";
 import Github from "./github.svg";
 import LinkedIn from "./linkedin.svg";
 
-export { NicolasLogo, Github, LinkedIn };
+import CSS from "./skills/css.svg";
+import Django from "./skills/django.svg";
+import HTML from "./skills/html.svg";
+import Javascript from "./skills/javascript.svg";
+import Sass from "./skills/sass.svg";
+import Tailwind from "./skills/tailwind.svg";
+import Typescript from "./skills/typescript.svg";
+import React from "./skills/react.svg";
+import Next from "./skills/next.svg";
+import Figma from "./skills/figma.svg";
+import Trello from "./skills/trello.svg";
+import Notion from "./skills/notion.svg";
+import AzureDevOps from "./skills/azure-devops.svg";
+import GraphQL from "./skills/graphql.svg";
+
+export {
+  NicolasLogo,
+  Github,
+  LinkedIn,
+  CSS,
+  Django,
+  HTML,
+  Javascript,
+  Sass,
+  Tailwind,
+  Typescript,
+  React,
+  Next,
+  Figma,
+  Trello,
+  Notion,
+  AzureDevOps,
+  GraphQL,
+};
