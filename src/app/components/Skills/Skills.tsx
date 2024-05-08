b53f29665352b38207e9db6689e4651582b284e4
Nicolas <nicolasarrastia@gmail.com>
2024-05-07 21:14:44 -0300

    Add bigger gap

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -10,7 +10,7 @@ const Skills = () => {
     <SectionLayout id={SectionIds.SKILLS}>
       <TitleText text="My Skill Set" />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
         {SKILLS.map(({ title, skills }) => (
           <div key={title}>
             <div className="flex relative">
