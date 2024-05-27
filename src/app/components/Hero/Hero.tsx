f0b89ef2c745f201579235ccc6ca1dc891cc4365
Nicolas <nicolasarrastia@gmail.com>
2024-05-27 11:54:52 -0300

    Design adjustments

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -10,13 +10,13 @@ const Hero = () => {
         src={GreatWaveOfKanagawa}
         alt={"great wave of kanagawa background"}
       />
-      <div className="flex justify-center items-center bg-black opacity-60 absolute top-0 left-0 h-full w-full" />
+      <div className="flex justify-center items-center bg-black/65 absolute top-0 left-0 h-full w-full" />
       <div className="flex flex-col scale-[2] text-neutral-50">
-        <span className="text-sm">Hi there!</span>
+        <span className="text-sm text-neutral-300">Hi there!</span>
         <span className="text-4xl">
           I&apos;m <span className="text-blue-500 font-bold">Nico</span>
         </span>
-        <span className="text-sm">Web Developer</span>
+        <span className="text-sm text-neutral-300">Web Developer</span>
       </div>
     </div>
   );
