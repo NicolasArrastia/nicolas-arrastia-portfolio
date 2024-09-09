f6aecff9309ffba765dd78037107678bba8a4098
Nicolas <nicolasarrastia@gmail.com>
2024-09-09 13:10:17 -0300

    fix label content

--- src/app/components/Header/constants.ts
+++ src/app/components/Header/constants.ts
@@ -1,28 +1,29 @@
 import { SectionIds } from "@/app/enum";
+import { HeaderSections } from "@/lang";
 
 export const THRESHOLD = 100;
 
 export const HEADER_HEIGHT = 47.53;
 
-export const NAV_OPTIONS: { label: string; href: SectionIds }[] = [
+export const NAV_OPTIONS: { label: HeaderSections; href: SectionIds }[] = [
   {
-    label: "about",
+    label: HeaderSections.ABOUT,
     href: SectionIds.ABOUT,
   },
   {
-    label: "projects",
+    label: HeaderSections.PROJECTS,
     href: SectionIds.PROJECTS,
   },
   {
-    label: "skills",
+    label: HeaderSections.SKILLS,
     href: SectionIds.SKILLS,
   },
   {
-    label: "contact",
+    label: HeaderSections.CONTACT,
     href: SectionIds.CONTACT,
   },
   {
-    label: "experience",
+    label: HeaderSections.EXPERIENCE,
     href: SectionIds.EXPERIENCE,
   },
 ];
