8d6b7a4ef0a9a7e217e9a6bff43b98ca3213dafb
Nicolas <nicolasarrastia@gmail.com>
2024-09-09 13:10:05 -0300

    add lang types

--- src/lang.ts
+++ src/lang.ts
@@ -5,6 +5,54 @@ export enum Languages {
   EN = "en",
 }
 
-type LanguageJsonType = Record<Languages, any>; // todo develop more this type
+export enum HeaderSections {
+  ABOUT = "about",
+  PROJECTS = "projects",
+  SKILLS = "skills",
+  CONTACT = "contact",
+  EXPERIENCE = "experience",
+}
+
+type LangHeader = Record<HeaderSections, string>;
+
+type LangHero = {
+  title: string[];
+};
+
+type LangAbout = Record<
+  "introduction" | "why_me" | "random_facts",
+  { title: string; description: string[] }
+>;
+
+type LangSkills = {
+  title: string;
+  end_text: string;
+};
+
+type LangExperience = {
+  title: string;
+};
+
+type LangProjects = {
+  title: string;
+  demo_button: string;
+  in_progress: string;
+};
+
+type LangContact = {
+  title: string;
+};
+
+type LangContent = {
+  header: LangHeader;
+  hero: LangHero;
+  about: LangAbout;
+  skills: LangSkills;
+  experience: LangExperience;
+  projects: LangProjects;
+  contact: LangContact;
+};
+
+type LanguageJsonType = Record<Languages, LangContent>;
 
 export default lang as LanguageJsonType;
